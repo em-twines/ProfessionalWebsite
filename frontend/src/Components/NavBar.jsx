9194341f09091003dcd69fcb521d42a0c35eb458
emilytwines <emdelbridge@gmail.com>
2023-01-21 18:00:59 -0500

    fixed positioning of navbar in small display

--- frontend/src/Components/NavBar.jsx
+++ frontend/src/Components/NavBar.jsx
@@ -1,7 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import {
   MDBContainer,
   MDBCollapse,
@@ -9,30 +8,35 @@ import {
   MDBNavbarToggler,
   MDBIcon,
   MDBBtn,
-} from 'mdb-react-ui-kit';
-
+} from "mdb-react-ui-kit";
+import { Button, ButtonToolbar } from "react-bootstrap";
 
 export default function NavBar() {
   const [showAnimated2, setShowAnimated2] = useState(false);
 
-
   return (
     <>
-      <ul id = 'navFlex'>
+      <ul id="navFlex">
         <li className="brand">
           <Link to="/" style={{ textDecoration: "none", color: "white" }}>
-            <b className='nav'>HOME</b>
+            <b className="nav">HOME</b>
           </Link>
         </li>
-        <div className = 'nav-flex-jr'>
+        <div className="nav-flex-jr">
           <li className="projects-nav-link">
-            <Link to="/projects" style={{ textDecoration: "none", color: "white" }}>
-              <b className='nav'>PROJECTS</b>
+            <Link
+              to="/projects"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <b className="nav">PROJECTS</b>
             </Link>
           </li>
           <li className="about">
-            <Link to="/about" style={{ textDecoration: "none", color: "white" }}>
-              <b className='about'>ABOUT</b>
+            <Link
+              to="/about"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              <b className="about">ABOUT</b>
             </Link>
           </li>
           {/* <li className="contact-nav-link">
@@ -43,21 +47,27 @@ export default function NavBar() {
         </div>
       </ul>
 
-
-      <div id = 'nav-burger'>
-        <section className='mb-3'>
-          <MDBNavbar dark bgColor='dark'>
+      <div id="nav-burger">
+        <section className="mb-3 ">
+          <MDBNavbar   dark bgColor="dark">
             <MDBContainer fluid>
+              <Link
+                to="/"
+                className="home"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                HOME
+              </Link>
               <MDBNavbarToggler
-                type='button'
-                className='second-button'
-                data-target='#navbarToggleExternalContent'
-                aria-controls='navbarToggleExternalContent'
-                aria-expanded='false'
-                aria-label='Toggle navigation'
+                type="button"
+                className="second-button"
+                data-target="#navbarToggleExternalContent"
+                aria-controls="navbarToggleExternalContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
                 onClick={() => setShowAnimated2(!showAnimated2)}
               >
-                <div className={`animated-icon2 ${showAnimated2 && 'open'}`}>
+                <div className={`animated-icon2 ${showAnimated2 && "open"}`}>
                   <span></span>
                   <span></span>
                   <span></span>
@@ -67,15 +77,26 @@ export default function NavBar() {
             </MDBContainer>
           </MDBNavbar>
 
-          <MDBCollapse show={showAnimated2}>
-          
-            <div className='bg-light shadow-3 p-4'>
-              <MDBBtn block className='border-bottom m-0 nav-bar-link' >
-                PROJECTS
-              </MDBBtn>
-              <MDBBtn block className='border-bottom m-0 nav-bar-link'>
-                ABOUT
-              </MDBBtn>
+          <MDBCollapse show={showAnimated2} style={{ position: "absolute", right: '0px'}}className="align-right">
+            <div className="shadow-3 p-4">
+              <Link
+                to="/projects"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <Button className=" border-bottom m-1 nav-bar-link ">
+                  PROJECTS{" "}
+                </Button>
+              </Link>
+
+              <Link
+                to="/about"
+                style={{ textDecoration: "none", color: "white" }}
+              >
+                <Button className="border-bottom m-1 nav-bar-link">
+                  ABOUT{" "}
+                </Button>
+              </Link>
+
               {/* <MDBBtn block className='border-bottom m-0' color='link'>
                 CONTACT
               </MDBBtn> */}
@@ -84,23 +105,11 @@ export default function NavBar() {
         </section>
       </div>
     </>
-
   );
 }
 
-
-
-
-
-
-
-
-
-
-
 // import React from 'react'
 // import { Link } from 'react-router-dom';
-
 
 // export default function NavBar() {
 //   return (

2e96861352dd572a3fd6f7fe7651feb0cfec64fa
emilytwines <emdelbridge@gmail.com>
2023-02-15 16:50:24 -0500

    changed desktop layout on projects page by adding a horizontal flex

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -42,9 +42,9 @@ export default function Projects() {
 
       <div className="projects-flex">
         <div className="section-title ">Projects</div>
-        <div className="project-page-description">
-          A short selection of recent work.
-        </div>
+        {/* <div className="project-page-description">
+          A short selection of my work.
+        </div> */}
         <hr></hr>
         <div className="project">
           <div className="project-title">
@@ -54,44 +54,47 @@ export default function Projects() {
           <div className="text-muted subtitle">
             An All-In-One PTO Management System
           </div>
-          <a href="https://github.com/em-twines/same-page-time-tracker">
-            <div
-              className="project-image img1"
-              alt="Preliminary Procreate-sketch for Same Page app"
-            ></div>
-          </a>
-          <div className="column-container">
-            <div className="column-1">
+          <div className="horizontal-column">
+            <a href="https://github.com/em-twines/same-page-time-tracker">
+              <div
+                className="img img1"
+                alt="Preliminary Procreate-sketch for Same Page app"
+              ></div>
+            </a>
+
+            <div className="column-container">
+              <div className="column-1">
+                <ul className="ul-projects">
+                  <li className="list">React</li>
+                  <li className="list">JWT</li>
+                  <li className="list">Django REST</li>
+                  <li className="list">FullCalendar.io</li>
+                  <li className="list">MySQL</li>
+                </ul>
+              </div>
+              <div className="column-1">
+                <ul className="ul-projects">
+                  <li className="list">HTML</li>
+                  <li className="list">CSS</li>
+                  <li className="list">Javascript</li>
+                  <li className="list">Python</li>
+                </ul>
+              </div>
+            </div>
+            <div className="one-column">
               <ul className="ul-projects">
                 <li className="list">React</li>
                 <li className="list">JWT</li>
                 <li className="list">Django REST</li>
                 <li className="list">FullCalendar.io</li>
                 <li className="list">MySQL</li>
-              </ul>
-            </div>
-            <div className="column-1">
-              <ul className="ul-projects">
+
                 <li className="list">HTML</li>
                 <li className="list">CSS</li>
                 <li className="list">Javascript</li>
                 <li className="list">Python</li>
               </ul>
             </div>
-          </div>
-          <div className="one-column">
-            <ul className="ul-projects">
-              <li className="list">React</li>
-              <li className="list">JWT</li>
-              <li className="list">Django REST</li>
-              <li className="list">FullCalendar.io</li>
-              <li className="list">MySQL</li>
-
-              <li className="list">HTML</li>
-              <li className="list">CSS</li>
-              <li className="list">Javascript</li>
-              <li className="list">Python</li>
-            </ul>
           </div>
         </div>
         <hr></hr>
@@ -100,128 +103,131 @@ export default function Projects() {
           <div className="text-muted subtitle ">
             Visualizations of a Game-based Dataset
           </div>
-
-          <a href="https://github.com/GriffithT16/VideoGame_frontend">
-            <div
-              className="project-image img2"
-              alt="A data visualization project using Nintendo API"
-            ></div>
-          </a>
-          <div className="column-container">
-            <div className="column-1">
+          <div className="horizontal-column">
+            <a href="https://github.com/GriffithT16/VideoGame_frontend">
+              <div
+                className="img img2"
+                alt="A data visualization project using Nintendo API"
+              ></div>
+            </a>
+            <div className="column-container">
+              <div className="column-1">
+                <ul className="ul-projects">
+                  <li className="list">React</li>
+                  <li className="list">Java</li>
+                  <li className="list">Java Spring</li>
+                  <li className="list">Nintendo API</li>
+                </ul>
+              </div>
+              <div className="column-1">
+                <ul className="ul-projects">
+                  <li className="list">HTML</li>
+                  <li className="list">CSS</li>
+                  <li className="list">Javascript</li>
+                </ul>
+              </div>
+            </div>
+            <div className="one-column">
               <ul className="ul-projects">
                 <li className="list">React</li>
                 <li className="list">Java</li>
                 <li className="list">Java Spring</li>
                 <li className="list">Nintendo API</li>
-              </ul>
-            </div>
-            <div className="column-1">
-              <ul className="ul-projects">
+
                 <li className="list">HTML</li>
                 <li className="list">CSS</li>
                 <li className="list">Javascript</li>
               </ul>
             </div>
           </div>
-          <div className="one-column">
-            <ul className="ul-projects">
-              <li className="list">React</li>
-              <li className="list">Java</li>
-              <li className="list">Java Spring</li>
-              <li className="list">Nintendo API</li>
-
-              <li className="list">HTML</li>
-              <li className="list">CSS</li>
-              <li className="list">Javascript</li>
-            </ul>
-          </div>
         </div>
+
         <hr></hr>
         <div className="project">
           <div className="project-title">Vid.io</div>
           <div className="text-muted subtitle">
             A Music Streaming Site Utilizing Youtube's API
           </div>
-          <a href="https://github.com/em-twines/YoutubeClone">
-            <div
-              className="project-image img3"
-              alt="A VideoPlayer app using Youtube's API"
-            ></div>
-          </a>
-
-          <div className="column-container">
-            <div className="column-1">
+          <div className="horizontal-column">
+            <a href="https://github.com/em-twines/YoutubeClone">
+              <div
+                className="img img3"
+                alt="A VideoPlayer app using Youtube's API"
+              ></div>
+            </a>
+            <div className="column-container">
+              <div className="column-1">
+                <ul className="ul-projects">
+                  <li className="list">React</li>
+                  <li className="list">JWT</li>
+                  <li className="list">YouTube API</li>
+                  <li className="list">MySQL</li>
+                </ul>
+              </div>
+              <div className="column-1">
+                <ul className="ul-projects">
+                  <li className="list">HTML</li>
+                  <li className="list">CSS</li>
+                  <li className="list">Javascript</li>
+                  <li className="list">Python</li>
+                </ul>
+              </div>
+            </div>
+            <div className="one-column">
               <ul className="ul-projects">
                 <li className="list">React</li>
                 <li className="list">JWT</li>
+                <li className="list">Django REST</li>
                 <li className="list">YouTube API</li>
-                <li className="list">MySQL</li>
-
-              </ul>
-            </div>
-            <div className="column-1">
-              <ul className="ul-projects">
                 <li className="list">HTML</li>
                 <li className="list">CSS</li>
                 <li className="list">Javascript</li>
                 <li className="list">Python</li>
               </ul>
             </div>
-          </div>
-          <div className="one-column">
-            <ul className="ul-projects">
-              <li className="list">React</li>
-              <li className="list">JWT</li>
-              <li className="list">Django REST</li>
-              <li className="list">YouTube API</li>
-              <li className="list">HTML</li>
-              <li className="list">CSS</li>
-              <li className="list">Javascript</li>
-              <li className="list">Python</li>
-            </ul>
           </div>
         </div>
         <hr></hr>
         <div className="project">
           <div className="project-title">Memory Cards</div>
           <div className="text-muted subtitle">A Flashcards App</div>
-          <a href="https://github.com/em-twines/flashcards">
-            <div
-              className="project-image img4"
-              alt="A customizable flashcards app"
-            ></div>
-          </a>
-          <div className="column-container">
-            <div className="column-1">
+          <div className="horizontal-column">
+            <a href="https://github.com/em-twines/flashcards">
+              <div
+                className="img img4"
+                alt="A customizable flashcards app"
+              ></div>
+            </a>
+            <div className="column-container">
+              <div className="column-1">
+                <ul className="ul-projects">
+                  <li className="list">React</li>
+                  <li className="list">Django REST</li>
+                  <li className="list">Python</li>
+                </ul>
+              </div>
+              <div className="column-1">
+                <ul className="ul-projects">
+                  <li className="list">HTML</li>
+                  <li className="list">CSS</li>
+                  <li className="list">Javascript</li>
+                </ul>
+              </div>
+            </div>
+            <div className="one-column">
               <ul className="ul-projects">
                 <li className="list">React</li>
                 <li className="list">Django REST</li>
                 <li className="list">Python</li>
-              </ul>
-            </div>
-            <div className="column-1">
-              <ul className="ul-projects">
                 <li className="list">HTML</li>
                 <li className="list">CSS</li>
                 <li className="list">Javascript</li>
               </ul>
             </div>
           </div>
-          <div className="one-column">
-            <ul className="ul-projects">
-              <li className="list">React</li>
-              <li className="list">Django REST</li>
-              <li className="list">Python</li>
-              <li className="list">HTML</li>
-              <li className="list">CSS</li>
-              <li className="list">Javascript</li>
-            </ul>
-          </div>
         </div>
-
-        <hr></hr>
       </div>
+      <hr></hr>
     </div>
   );
 }

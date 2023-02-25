ccd968f1c2008c3e8e09919128d1b2724673d2ab
emilytwines <emdelbridge@gmail.com>
2023-02-25 08:15:15 -0500

    edited text on About Me page, added a css class to support changes

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,8 +3,6 @@ import React from "react";
 export default function About() {
   return (
     <div>
-    
-   
       <div className="particle"></div>
       <div className="particle"></div>
       <div className="particle"></div>
@@ -25,31 +23,44 @@ export default function About() {
           <br></br>
           <hr></hr> <div className="about-me-image"> </div> <br></br>
           <hr></hr>
-          I'm Emily, and I write code.
+          <hr></hr>
+          <div className="tag-line">
+            A software engineer by way of performance art, I started learning by
+            teaching myself C# and Unity during lockdown.
+            <br></br>
+            <br></br>
+            Three months later, lookingGlass was live, and I had discovered a
+            new passion.
+          </div>
           <p className="about-me-text">
+            <hr></hr>
             <br></br>
             <hr></hr>
-            Before I wrote code, I made art -- programming for live interactive
-            performances and making games. I sort of think coding is art too. I
-            love a good puzzle and a detailed error message, and am always
-            excited to see a project through the finish line. I'm enthusiastic
-            and inquisitive, and just generally really enjoy doing what I do.
+            Hi, I'm Emily. I'm a Full-Stack developer, a former middle school
+            teacher, and an indie video game designer. Before I wrote code, I
+            made art -- programming for live interactive performances and making
+            games. I sort of think coding is art too. I love a good puzzle and a
+            detailed error message, and am always excited to see a project
+            through the finish line. I'm enthusiastic and inquisitive, and just
+            generally enjoy doing what I do.
             <br></br>
             <hr></hr>I started considering moving more into the tech world after
-            finishing my MFA thesis project, lookingGlass, in 2020. I had been
-            working for nine months on a live performance, but in March, the
-            world changed. I didn't want to do a staged reading or something
+            finishing my MFA thesis project, lookingGlass, in May of 2020. I had
+            been working for nine months on a live performance, but in March,
+            the world changed. I didn't want to do a staged reading or something
             pre-recorded that would stream on Zoom, so I decided to translate
             the show using the vocabulary of video games. I learned how to code
             in C# and how to use Unity, how to manage a server and how to host
-            web content. Three months later, the game was live, and I had
-            discovered a new passion.
+            web content. By the day of the performance, lookingGlass was live,
+            and a new door for me was opened.
             <hr></hr>
-            <br></br>I just graduated from an intensive Full-Stack boot-camp,
-            and am looking for opportunities that will help me grow as a
-            developer. I love working in React and everything about styling the
-            frontend and crafting the UI, but I also really like the challenge
-            of sculpting data that I learned to do in Django/Spring.
+            <br></br>In October I took the next step, and enrolled in an
+            intensive Full-Stack boot-camp. As a graduate, I and am now looking
+            for opportunities that will help me grow as a developer. I love
+            working in React and everything about sending data around the
+            frontend and crafting the UI, but I also really enjoy sculpting the
+            data from the start in Django or Spring. I'm excited for what's next, and can't wait
+            to learn more!
             <br></br>
             <hr></hr> Thanks for checking out my portfolio, and feel free to
             reach out!
